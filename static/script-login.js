1bf0cc1b77ed6e029cb3786de2d40c83f90fcf72
AGUSTINUSBOWO <agustinusbowo698@gmail.com>
2026-01-27 10:52:08 +0700

    Update project

--- static/script-login.js
+++ static/script-login.js
@@ -7,22 +7,28 @@ document.getElementById('loginForm').addEventListener('submit', function(e) {
     const container = document.querySelector('.login-container');
     const splash = document.getElementById('splashScreen');
 
-    // Validasi Sederhana (Sesuai Permintaan)
+    // Validasi Sederhana
     if (u === 'SDMK2026' && p === 'SDMK11234#') {
         // SUKSES
         err.style.display = 'none';
+        
+        // Sembunyikan form login agar transisi lebih rapi
+        container.style.display = 'none'; 
+        
+        // Tampilkan Splash Screen
         splash.classList.remove('hidden');
         
         // Timer Splash Screen sebelum redirect
         setTimeout(() => {
-            window.location.href = '/kuota';
+            // PERBAIKAN DI SINI:
+            // Gunakan 'window.top.location.href' untuk keluar dari iframe Google
+            window.top.location.href = 'https://script.google.com/macros/s/AKfycbz-FwJ2ev_P-9BuYtDazV6grZ5ysIMWyrOi_EbLv9s/dev';
         }, 2000);
     } else {
         // GAGAL
         err.style.display = 'flex';
         container.classList.add('shake');
         
-        // Hapus animasi shake biar bisa di-trigger lagi
         setTimeout(() => {
             container.classList.remove('shake');
         }, 500);
